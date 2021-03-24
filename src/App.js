7c803e24a108cd8c89f79880f552cd0212b0305b
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-03-24 09:28:35 -0300

    add update item quantity, remove item and empty cart functionalities

--- src/App.js
+++ src/App.js
@@ -23,6 +23,24 @@ const App = () => {
 		setCart(item.cart)
 	}
 
+	const handleUpdateCartQty = async (productId, quantity) => {
+		const { cart } = await commerce.cart.update(productId, { quantity })
+
+		setCart(cart)
+	}
+
+	const handleRemoveFromCart = async (productId) => {
+		const { cart } = await commerce.cart.remove(productId)
+
+		setCart(cart)
+	}
+
+	const handleEmptyCart = async () => {
+		const { cart } = await commerce.cart.empty()
+
+		setCart(cart)
+	}
+
 	useEffect(() => {
 		fetchProducts()
 		fetchCart()
@@ -38,7 +56,12 @@ const App = () => {
 					</Route>
 
 					<Route exact path="/cart">
-						<Cart cart={ cart } />
+						<Cart 
+							cart={ cart } 
+							handleUpdateCartQty={ handleUpdateCartQty }
+							handleRemoveFromCart={ handleRemoveFromCart }
+							handleEmptyCart={ handleEmptyCart }
+						/>
 					</Route>	
 				</Switch>
 			</div>
