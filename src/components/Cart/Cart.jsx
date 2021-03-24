4558a90f5c64bfaff75bf28dcb49742f136bbc15
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-03-24 08:59:45 -0300

    add empty cart link

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core'
-import CartItem from './CartItem/CartItem'
+import { Link } from 'react-router-dom'
 
+import CartItem from './CartItem/CartItem'
 import useStyles from './styles'
 
 const Cart = ({ cart }) => {
     const classes = useStyles()
 
     const EmptyCart = () => {
-        <Typography variant="subtitle1"> You have no items in your shopping cart, start adding soem! </Typography>
+        <Typography variant="subtitle1"> You have no items in your shopping cart, start adding soem! 
+            <Link to="/" className={ classes.link }> start adding some! </Link>
+        </Typography>
     }
 
     const FilledCart = () => (
